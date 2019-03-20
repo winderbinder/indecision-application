c6cfd4b63812419552fff65df57093830bb0ff3f
winderbinder <junior_19000@hotmail.com>
2019-03-19 20:04:34 -0600

    created 2 variables thatand the reactdom environment

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,7 @@
 console.log('App.js is running!')
 
 // JSX - Javascript Xml
+var template = <p>This is JSX from app.js!</p>;
+var appRoot = document.getElementById("app");
+
+ReactDom.render(template, appRoot);
